9a2aeca4ad471f06efce60276114e878c5f3e2a9
ömer turan <126421999+omerturan-developer@users.noreply.github.com>
2023-06-20 00:35:31 +0300

    Add files via upload

---
+++ react-xox game/game/src/App.js
@@ -0,0 +1,15 @@
+import './App.css';
+import Sandbox from './Sandbox';
+
+
+function App() {
+  return (
+    <div className="App">
+      <h2>XOX OYUNU</h2>
+      <Sandbox/>
+    </div>
+  );
+}
+
+
+export default App;
